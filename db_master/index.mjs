bca71cb78ca183e18e18eba074fe685c56bfcbf3
madhav <guptamadhav2775@gmail.com>
2023-06-27 01:42:21 +0530

    implemented few events

--- db_master/index.mjs
+++ db_master/index.mjs
@@ -13,8 +13,8 @@ const [udp, tcp] = [
   get_tcp_socket(server_name, heartbeat_port)
 ];
 
-// Create a list of active connections
-const active_servers = new Set();
+// maintain a map of active servers and their chances i.e IP:PORT -> chance
+const active_servers = new Map();
 
 // Register
 tcp.on('connection', sock => {
@@ -27,7 +27,7 @@ tcp.on('connection', sock => {
   // register a database
   sock.on('data', data => {
     if (data == "Connect me as database!") {
-      active_servers.add(IP);
+      active_servers.set(name, 10);
       sock.write('Registered as database node.');
       sock.destroy();
     } else {
@@ -38,8 +38,26 @@ tcp.on('connection', sock => {
   sock.on('close', _ => console.log(`Connection closed with ${name}\n`));
 });
 
+// send heartbeat to all active servers every 5 seconds
+setInterval(_ => {
+  active_servers.forEach((chance, name) => {
+    if (chance == 0) {
+      console.log(`database ${name} deleted.`)
+      active_servers.delete(name);
+    } else {
+      console.log(`Sending heartbeat to ${name}`);
+      udp.send("Heartbeat", name.split(':')[1], name.split(':')[0]);
+      console.log(`data sent: Heartbeat, ${name.split(':')[1]}, ${name.split(':')[0]}`);
+      active_servers.set(name, chance - 1);
+    }
+  });
+}, 5000);
 
-// Heartbeat
+
+// Heartbeat response handler
 udp.on('data', (msg, rinfo) => {
   console.log(`UDP got: ${msg} from ${rinfo.address}:${rinfo.port}\n`);
-});
+  if (msg == "Heartbeat") {
+    active_servers.set(`${rinfo.address}:${rinfo.port}`, 10);
+  }
+}); 
--- db_node/index.mjs
+++ db_node/index.mjs
@@ -1,5 +1,8 @@
 'use strict';
 import * as net from 'net';
+import * as dgram from 'dgram';
+import * as os from 'os';
+import { get } from 'http';
 const heartbeat_port = process.env.HEARTBEAT_PORT ?? 8085;
 const server_name = process.env.SERVER_NAME ?? 'localhost';
 
@@ -13,3 +16,47 @@ socket.on('data', data => console.log(`Server Response : ${data}`));
 socket.on('close', err => console.log("Closed\n"));
 
 socket.on('error', err => console.log(err));
+
+
+// store own ip address in a variable
+const get_ip = _ => {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+}
+console.log(get_ip());
+
+
+// store udp socket in a variable
+// const udp_socket = dgram.createSocket('udp4');
+// console.log(`UDP socket created.`);
+// udp_socket.bind(heartbeat_port);
+
+// create socket
+const udp_socket = dgram.createSocket('udp4');
+console.log(`UDP socket created.`);
+
+// start udp server and bind it to the port
+udp_socket.bind(heartbeat_port, () => {
+  const address = udp_socket.address();
+  console.log(`Heartbeat UDP Process started on ${address.address}:${address.port}`);
+});
+    
+// event listener in case of error
+udp_socket.on('error', err => {
+  console.log(`UDP socket error:\n${err.stack}`);
+  udp_socket.close();
+});
+
+udp_socket.on('message', (msg, rinfo_ip, rinfo_port) => {
+  console.log(`UDP got: ${msg} from ${rinfo_ip}:${rinfo_port}\n`);
+  if (msg == "Heartbeat") {
+    udp_socket.send("Heartbeat", rinfo_port, rinfo_ip);
+    console.log(`data sent: Heartbeat, ${rinfo_port}, ${rinfo_ip}`);
+  }
+});
